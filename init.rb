2154ae75996d4f6e1a5b92179dff246d394e7053
MAEDA Go <maeda@farend.jp>
2011-07-19 17:11:37 +0900

    changed plugin properties (plugin name, author info).

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_vividtone_my_page_blocks do
-  name 'Redmine Vividtone My Page Blocks plugin'
+  name 'My Page Blocks plugin'
   author 'MAEDA Go (FAR END Technologies)'
   description 'Add some my page blocks'
   version '0.0.1'
   url 'http://github.com/vividtone/redmine_vividtone_my_page_blocks'
-  author_url ''
+  author_url 'http://twitter.com/g_maeda'
 end
