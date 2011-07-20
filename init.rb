e142fe30a7504b90e7c94d2d13879083836c6c86
MAEDA Go <maeda@farend.jp>
2011-07-20 20:19:10 +0900

    version 1.0 release.

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_vividtone_my_page_blocks do
   name 'My Page Blocks plugin'
   author 'MAEDA Go (FAR END Technologies)'
   description 'Add some my page blocks'
-  version '0.0.1'
+  version '1.0 (20110720)'
   url 'http://github.com/vividtone/redmine_vividtone_my_page_blocks'
   author_url 'http://twitter.com/g_maeda'
 end
