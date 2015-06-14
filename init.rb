84613a14e587f65540a70146b0904c0cb07ea441
MAEDA Go <maeda@farend.jp>
2015-06-14 12:19:55 +0900

    The plugin now requires Redmine 3.0.

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_vividtone_my_page_blocks do
+  requires_redmine version_or_higher: '3.0.0'
   name 'My Page Blocks plugin'
   author 'MAEDA Go (FAR END Technologies)'
   description 'Add some my page blocks'
