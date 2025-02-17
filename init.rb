1137764f41f51df31805e387c1fa75f57da2405f
Mizuki Ishikawa <remember929@gmail.com>
2025-02-17 15:33:45 +0900

    Update the plugin version from 1.3 to 1.4

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_vividtone_my_page_blocks do
   name 'My Page Blocks plugin'
   author 'MAEDA Go (FAR END Technologies)'
   description 'Add some my page blocks'
-  version '1.3'
+  version '1.4'
   url 'http://blog.redmine.jp/articles/my-page-blocks-plugin/'
   author_url 'https://twitter.com/g_maeda'
 end
